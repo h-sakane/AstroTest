51e906d5743347d506520d5011aa92d7198419ee
h-sakane <h-sakane@bns-g.com>
2025-03-05 15:14:13 +0900

    Github Actions用の設定を追記

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://<username>.github.io',
+  site: 'https://h-sakane.github.io',
   base: '/',
   integrations: [tailwind()],
 });
